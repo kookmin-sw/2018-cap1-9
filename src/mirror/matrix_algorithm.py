26b6193f13aefe250350cf3e20bb16eb6483b3b3
wlsdud <wlsdud5938@naver.com>
2018-05-22 12:27:22 +0900

    데이터베이스에서 얻어온 정보 정재
    상의 옷색

--- src/mirror/matrix_algorithm.py
+++ src/mirror/matrix_algorithm.py
@@ -12,13 +12,10 @@ sql = "select * from Clothes_Info"
 curs.execute(sql)
 
 rows = curs.fetchall()
-print(rows[0][2])
-for i in rows:
-    print(i)
 
-m1 = [[1, 4, 1, 1], [4, 3, 3, 3], [3, 4, 2, 1], [4, 1, 3, 4], [2, 3, 2, 1]]
-m2 = [[3, 4, 4, 3, 1, 4, 4, 3, 2], [4, 2, 1, 4, 3, 4, 3, 3, 2], [3, 3, 2,	2, 4, 4, 2,	3, 1],
-      [1, 1, 3, 2, 4,	3,	3,	1, 1], [2,	3, 4, 3, 2,	4,	2,	2, 3]]
+m1 = [[1, 4, 1, 1,2], [4, 3, 3, 3,2], [3, 4, 2, 1,2], [4, 1, 3, 4,2], [2, 3, 2, 1,2],[2,2,2,2,2]]
+m2 = [[3, 4, 4, 3, 1, 4, 4, 3, 2,2], [4, 2, 1, 4, 3, 4, 3, 3, 2,2], [3, 3, 2,	2, 4, 4, 2,	3, 1,2],
+      [1, 1, 3, 2, 4,	3,	3,	1, 1,2], [2,	3, 4, 3, 2,	4,	2,	2, 3,2],[2,2,2,2,2,2,2,2,2,2]]
 
 
 def summ2(list1, list2):
@@ -28,14 +25,40 @@ def summ2(list1, list2):
     b1 = list2[1]
     return m1[a1][a] + m2[b1][b]
 
+def changeColorUp(color):
+    if(color == "Green"):
+        return 0
+    elif(color == "Blue"):
+        return 1
+    elif (color == "Yellow"):
+        return 2
+    elif (color == "Pink"):
+        return 3
+    elif (color == "Navy"):
+        return 4
+    elif (color == "White"):
+        return 5
+    elif (color == "Black"):
+        return 6
+    elif (color == "grey"):
+        return 7
+    else:
+        return 9
 
+
+chooseCloth = input("어떤 옷을 선택하시겠습니까? : ")
 l1 = [[1, 6], [2, 5], [0, 3], [3, 7]]
 l2 = [[0, 3], [0, 4], [1, 4], [3, 4], [4, 3], [2, 2]]
 
-select = l1[0]
+select = l1[chooseCloth]
 list3 = []
-for i in l2:
-    sum1 = summ2(select, i)
-    list3.append(sum1)
+if(chooseCloth < =9):
+    for i in l2:
+        sum1 = summ2(select, i)
+        list3.append(sum1)
+else:
+    for i in l1:
+        sum1 = summ2(select, i)
+        list3.append(sum1)
 
 print(list3)
