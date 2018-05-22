af25d96a098891072a0bf159539bff5ab9b0f5ea
wlsdud <wlsdud5938@naver.com>
2018-05-22 12:54:58 +0900

    데이터 상,하의 종류 데이터 정제

--- src/mirror/matrix_algorithm.py
+++ src/mirror/matrix_algorithm.py
@@ -45,7 +45,19 @@ def changeColorUp(color):
     else:
         return 9
 
-def changeColorDonw(color):
+def changeKindUp(Kind):
+    if (Kind == "Blouse"):
+        return 0
+    elif (Kind == "Tshrit"):
+        return 1
+    elif (Kind == "Shrit"):
+        return 2
+    elif (Kind == "Suit"):
+        return 3
+    else:
+        return 4
+
+def changeColorDown(color):
     if(color == "Dark Blue"):
         return 0
     elif (color == "Light Blue"):
@@ -59,6 +71,19 @@ def changeColorDonw(color):
     else:
         return 5
 
+def changeKindDown(Kind):
+    if (Kind == "Active Wear"):
+        return 0
+    elif (Kind == "Skirt"):
+        return 1
+    elif (Kind == "Jean"):
+        return 2
+    elif (Kind == "Suit"):
+        return 3
+    elif (Kind == "Mini Skirt"):
+        return 4
+    else:
+        return 5
 
 chooseCloth = input("어떤 옷을 선택하시겠습니까? : ")
 l1 = [[1, 6], [2, 5], [0, 3], [3, 7]]
@@ -66,6 +91,14 @@ l2 = [[0, 3], [0, 4], [1, 4], [3, 4], [4, 3], [2, 2]]
 
 select = l1[chooseCloth]
 list3 = []
+'''
+topList = {}
+pantsList = {}
+
+for i in rows[0]:
+    if(i[0] <= 9):
+        topList[i[0]] = [changeColorUp(i[1]),i[4]]
+'''
 if(chooseCloth < =9):
     for i in l2:
         sum1 = summ2(select, i)
