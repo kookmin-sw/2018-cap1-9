a9503b18fac34b24fc1fb048ae43c194b97c1597
Kim doeun <doeuncow@naver.com>
2018-05-26 15:11:34 +0900

    #37-complete db connecting example and complete aws connecting and upload image at aws s3

---
+++ src/hanger/picamera/dbconnect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import MySQLdb
+
+db = MySQLdb.connect(host="34.225.233.100",
+                     user="root",
+                     passwd="1234",
+                     db="VT")
+cur = db.cursor()
+
+cur.execute("SELECT * FROM Clothes_Info")
+
+for row in cur.fetchall():
+    print(row[1])
+    
+db.close()
---
+++ src/hanger/picamera/uploadfile.py
@@ -0,0 +1,30 @@
+import sys, os, glob
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+
+AWS_ACCESS = 'AKIAI6RCJX5WEYMBNMVA'
+AWS_SECRET = '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'
+
+conn = S3Connection(AWS_ACCESS, AWS_SECRET)
+bucket = conn.get_bucket('clothes-image')
+directory = '/home/pi/Desktop/picamera'
+
+def getFiles(dir):
+    return [os.path.basename(x) for x in glob.glob(str(dir)+'*.jpg')]
+
+def upload_S3(dir,file):
+    k = Key(bucket)
+    k.key = f
+    k.set_contents_from_filename(dir + f)
+    
+def removeLocal(dir,file):
+    os.remove(dir+file)
+    
+filenames = getFiles(directory)
+print(filenames)
+
+for f in filenames:
+    print('Uploading %s to Amazon S3 bucket %s' % (f,bucket))
+    upload_S3(directory, f)
+    removeLocal(directory, f)
+    
