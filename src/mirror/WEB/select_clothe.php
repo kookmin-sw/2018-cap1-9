f4000c80377a8fe263c45dcd9f62f9ad048c03cf
daianaludice <daianaludice@naver.com>
2018-05-29 00:33:02 -0700

    #39 select_clthe Front-End modify

--- src/mirror/WEB/select_clothe.php
+++ src/mirror/WEB/select_clothe.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 $servername = "localhost";
 $username = "root";
 $password = "1234";
@@ -33,12 +33,21 @@ if($position == 'lower'){
     else{ echo "Error: " .$sql . "<br>" . mysqli_error($conn);
     }
 }
-
 exec("python matrix_algorithm.py");
 
 
+$que = "SELECT show_addr FROM Coordinate where position='upper'" ;
+$res3 =  mysqli_query($conn,$que);
+$row3 =  mysqli_fetch_array($res3);
+$upper_addr = $row3[0];
+
+$que2 = "SELECT show_addr FROM Coordinate where position='lower'" ;
+$res4 =  mysqli_query($conn,$que2);
+$row4 =  mysqli_fetch_array($res4);
+$lower_addr = $row4[0];
 
 ?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -49,16 +58,54 @@ exec("python matrix_algorithm.py");
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
     <script type="text/javascript" src="http://code.jquery.com/jquery-2.2.3.min.js"></script>
+<style>
+    .button_css{
+     font-family: Arial, Helvetica, sans-serif;
+     font-size: 14px;
+     color: #ffffff;
+     padding: 2px 20px;
+     background: -moz-linear-gradient(
+       top,
+       #6b6b6b 0%,
+       #000000);
+     background: -webkit-gradient(linear, left top, left bottom,from(#6b6b6b),to(#000000));
+     -moz-border-radius: 30px;
+     -webkit-border-radius: 30px;
+     border-radius: 30px;
+     border: 1px solid #000000;
+     -moz-box-shadow:
+       0px 1px 3px rgba(000,000,000,0.5),
+       inset 0px 0px 1px rgba(255,255,255,0.7);
+     -webkit-box-shadow:
+       0px 1px 3px rgba(000,000,000,0.5),
+       inset 0px 0px 1px rgba(255,255,255,0.7);
+     box-shadow:
+       0px 1px 3px rgba(000,000,000,0.5),
+       inset 0px 0px 1px rgba(255,255,255,0.7);
+     text-shadow:
+       0px -1px 0px rgba(000,000,000,0.4),
+       0px 1px 0px rgba(255,255,255,0.3);
+    margin-left: 50%;
+    margin-top : 6%;
+    }
+    #choose:active {
+    color :blue;
+}
+    #cancle{
+
+    }
+    </style>
 </head>
-<body>
+<body style='background-color:white'>
 			<div id="wrapper">
 
 				<!-- Header -->
-					<header id="header">
-						<h1><a href="clothes_list.php"><- BACK </a></h1>
+					<header id="header" style="background-image:url(images/배경.png)" >
+						<h1><a href="clothes_list.php" style='color:black'><- clothes list </a></h1>
+            <span style='color:white; font-size:130%; margin-left:25%'> 현재 선택한 옷입니다. 고정하려면 버튼을 눌러주세요</span>
 						<nav>
 							<ul>
-								<li><a href="Insert_DB.php?No=<? echo"$No"; ?>"><button>WEAR</button></a></p></li>
+								<li><a href="Insert_DB.php?No=<? echo"$No";?>"><button>WEAR</button></a></p></li>
 
 							</ul>
 						</nav>
@@ -67,10 +114,28 @@ exec("python matrix_algorithm.py");
 				<!-- Main -->
 					<div id="main">
             <?php
-					echo "<img src='images/fulls/".$Addr.".jpg' class='image' width='device-width' height='device-height' style='padding-top:10%; padding-left:15%'/>";
+					echo "<div style='position:fixed; margin-left :8%;z-index : 4;'><img src='images/thumbs/".$upper_addr.".png' class='image' width='70%' style=' z-index:-1000; padding-left:3%'/></div>";
+          echo "<div style='position:fixed; margin-top:14%; margin-left :8%; z-index:1'><img src='images/thumbs/".$lower_addr.".png' class='image' width='65%'/></div>";
           ?>
+
 					</div>
+          <button class='button_css' id="choose" onclick="choose()">상의 고정</button>
+          <button class='button_css' id="cancle">상의 고정 취소</button>
+          <button class='button_css' id="choose" onclick="choose()">하의 고정</button>
+          <button class='button_css' id="cancle">상의 고정 취소</button>
+
+          <div style='position:fixed;margin-left:80%; background-color:black; margin-top:-37%; width:33%'>
+            <?php $fopen = fopen("list.txt", "r"); $list_1 = fgets($fopen); $list_2 = fgets($fopen); $list_3 = fgets($fopen); fclose($fopen);  ?>
+            <div>코디 추천</div>
+            <div style="color:white; font-size:140%">1순위<img src='images/thumbs/<? echo $list_1; ?>.png' width='40%'></div>
+            <div style="color:white; font-size:140%">2순위<img src='images/thumbs/<? echo $list_2; ?>.png' width='40%'></div>
+            <div style="color:white; font-size:140%">3순위<img src='images/thumbs/<? echo $list_3; ?>.png' width='40%'></div>
+          </div>
+<script>
+function choose() {
+    document.getElementById("choose").style.color = "red";
+}
+</script>
 			</div>
-      <script src="getParameter.js"></script>
 </body>
 <html>
