a27003d21b40c2ca1fca1c9caee0303a7f0330b7
daianaludice <daianaludice@naver.com>
2018-05-28 20:57:03 -0700

    #39 Clothes_list.php front-end modify

---
+++ src/mirror/WEB/VT_list.php
@@ -0,0 +1,75 @@
+﻿<?php
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$dbname = "VT";
+
+// Create connection
+ $conn = mysqli_connect($servername, $username, $password, $dbname);
+ // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Clothes List</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="Massets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="Massets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="Massets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="Massets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<?php
+		$query = 'SELECT count(No) FROM Clothes_Info';
+			$res = mysqli_query($conn,$query);
+						$row1 = $res -> fetch_array();
+					$total_num = $row1[0];
+											?>
+      <div style='opacity: 0.8;position:fixed;background-image:url(images/배경.png);text-align:center;color:white;font-size: 200%;vertical-align: baseline;width:100%;height:10%;z-index:10000;display: block '>
+        <span style="margin-left:-5%;margin-right:20%"><a href="main.php" style='text-decoration:none'><- BACK</a></span>
+        입어보고싶은 옷을 선택해주세요. <span style='margin-left:20%'>TOTAL<?php echo " $total_num"; ?></span></div>
+		<!-- Wrapper -->
+			<div id="wrapper">
+                         <h1 style='opacity:0'>'</h1>
+				<!-- Main -->
+				<?php
+				$query2 = 'SELECT No,Color,Kind,Picture_Addr FROM Clothes_Info';
+				$res2 =  mysqli_query($conn,$query2);
+					?>
+					
+<div id="main">
+						<div class="inner">
+							<header>
+	<h1>Clothes List </h1>
+								<p></p>
+							</header>
+							<section class="tiles">
+
+									<?php
+			            while ($row2 = $res2->fetch_assoc())
+                  {
+                    echo "<article class='style1'><span><img src='images/thumbs/".$row2['Picture_Addr'].".png' alt='' width='90%'/></span>";
+                    echo "<a href='select_clothe.php?No=".$row2['No']."&Picture_Addr=".$row2['Picture_Addr']."'><h2 style='color:black'>".$row2['Kind']."</h2><div class='content'>";
+                    echo "<p style='color:black'> Color :".$row2['Color']."  Type :".$row2['Kind']."</p></div> </a></article>";
+									}
+			            ?>
+							</section>
+						</div>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="Massets/js/jquery.min.js"></script>
+			<script src="Massets/js/skel.min.js"></script>
+			<script src="Massets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="Massets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="Massets/js/main.js"></script>
+
+	</body>
+</html>
--- src/mirror/WEB/main.php
+++ src/mirror/WEB/main.php
@@ -1,8 +1,8 @@
 ﻿<?php
 $servername = "localhost";
 $username = "root";
-$password = "autoset";
-$dbname = "vt";
+$password = "1234";
+$dbname = "VT";
 
 // Create connection
  $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -11,7 +11,7 @@ $dbname = "vt";
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$query2 = 'SELECT Picture_Addr FROM Clothes_info';
+$query2 = 'SELECT Picture_Addr FROM Clothes_Info';
 $res2 =  mysqli_query($conn,$query2);
 
 
@@ -27,28 +27,24 @@ $sql2 = "UPDATE Coordinate SET show_addr = NULL WHERE position = 'lower'";
     }
 
 
-
-
 ?>
-﻿<!DOCTYPE HTML>
+<!DOCTYPE HTML>
 <html>
 	<head>
-		<title>VT_MAIN</title>
+		<title>MAIN VT</title>
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
 		<!--[if lte IE 8]><script src="Oassets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="Oassets/css/main.css" />
+      <script type="text/javascript" src="http://code.jquery.com/jquery-2.2.3.min.js"></script>
 		<noscript><link rel="stylesheet" href="Oassets/css/noscript.css" /></noscript>
 		<!--[if lte IE 8]><link rel="stylesheet" href="Oassets/css/ie8.css" /><![endif]-->
-		<link rel="stylesheet" href="Oassets/css/add.css" />
 	</head>
 	<body>
-
 		<!-- Wrapper-->
 			<div id="wrapper">
-
 				<!-- Nav -->
-					<nav id="nav">
+		  		<nav id="nav">
 						<a href="#me" class="icon fa-home active"><span>Home</span></a>
 						<a href="#log" class="icon fa-folder"><span>Log</span></a>
 						<a href="#manual" class="icon fa-envelope"><span>Manual</span></a>
@@ -60,12 +56,13 @@ $sql2 = "UPDATE Coordinate SET show_addr = NULL WHERE position = 'lower'";
 						<!-- Me -->
 							<article id="me" class="panel">
 								<header style="margin-top:-6%">
-									<h1>VT PROJECT</h1>
+                  <h1>VT PROJECT</h1>
 									<p style="font-size:90%"><br>VT 프로젝트는 사용자가 가진 옷의 리스트들을 보여줍니다.<br>
 									   사용자가 원하는 옷을 선택하면 거울 위로 해당 옷이 나타납니다.<br>
 			               해당 옷을 입고 싶으시다면 <b><i>WEAR</i></b> 버튼을 누르고 옷장 문을 열어주세요.</p>
+
 								</header>
-								<a href="clothes_list.php" class="jumplink pic">
+								<a href="VT_list.php" class="jumplink pic">
 									<span class="arrow icon fa-chevron-right"><span>See my log</span></span>
 								</a>
 							</article>
@@ -81,7 +78,7 @@ $sql2 = "UPDATE Coordinate SET show_addr = NULL WHERE position = 'lower'";
 								</p>
 								<section>
 									<div class="row">
-										<?php
+<?php
 				            while ($row2 = $res2->fetch_assoc())
 
 										{
@@ -107,7 +104,7 @@ $sql2 = "UPDATE Coordinate SET show_addr = NULL WHERE position = 'lower'";
 
 		<!-- Scripts -->
 			<script src="Oassets/js/jquery.min.js"></script>
-			<script src="Oassets/js/skel.min.js"></script>
+<script src="Oassets/js/skel.min.js"></script>
 			<script src="Oassets/js/skel-viewport.min.js"></script>
 			<script src="Oassets/js/util.js"></script>
 			<!--[if lte IE 8]><script src="Oassets/js/ie/respond.min.js"></script><![endif]-->
